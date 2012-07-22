ab8363d575936215bd3735f95b0097e52fcd301c
Danny Castonguay <danny.castonguay@gmail.com>
2012-07-22 23:32:36 +0800

    Started created import scripts... On Arlo's suggestion

---
+++ django/bookkeeper/scImporter.py
@@ -0,0 +1,11 @@
+from bookkeeper.models import Coop
+
+file = open('coops.txt')
+
+for line in file:
+  columns = line.split(',')
+  coop = Coop()
+  coop.coopId = int(columns[0])
+  coop.save()
+
+file.close()
