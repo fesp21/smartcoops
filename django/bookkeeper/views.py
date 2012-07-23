1f4e26b57ffc405f160411da2307e23bfc040e63
Danny Castonguay <danny.castonguay@gmail.com>
2012-07-23 17:40:40 +0800

    decoupled url.py

--- django/bookkeeper/views.py
+++ django/bookkeeper/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+
+from bookkeeper.models import *
+from django.http import HttpResponse
+
+def index(request):
+    farmerList = Farmer.objects.all()
+    return render_to_response('bookkeeper/index.html', {'farmerList': farmerList})
+
--- django/sc/urls.py
+++ django/sc/urls.py
@@ -2,14 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'scdjango.views.home', name='home'),
-    # url(r'^scdjango/', include('scdjango.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+urlpatterns = patterns(
+    '',
+    url(r'^bookkeeper/', include('bookkeeper.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+    )
