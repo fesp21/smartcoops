3f2866f3cf06d9df88dbd18a6255c5d40f9d580b
dannycastonguay <danny.castonguay@gmail.com>
2012-07-26 00:18:56 +0800

    typos from sms tweet

--- farmbook/urls.py
+++ farmbook/urls.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'smstweet.views.show', name='home'),
+    url(r'^$', 'bookkeeper.views.show', name='home'),
     url(r'^bookkeeper/', include('bookkeeper.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^process/', 'smstweet.views.process'),
+    url(r'^process/', 'bookkeeper.views.process'),
     )
