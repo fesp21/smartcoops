db1c6f9a8af08942d87b3448fab2945fb543ac69
dannycastonguay <danny.castonguay@gmail.com>
2012-07-31 22:10:21 +0800

    renamed files

---
+++ farmbook/smsCommands/charge.py
@@ -0,0 +1,36 @@
+import re
+from farmbook.models import *
+
+def charge(entry):
+    """
+    Description:
+    When the coop disburses cash loans to member farmers; also used for when they incur expenses, eg. Xerox Php14.00
+
+    Fields:
+    Date
+    Name
+    Description (pre-filled in the voucher with "Amount payable", "Gasoline and oil", etc. but any will do)
+    Amount
+    Explanation (a more detailed description but can be combined with "Description")
+
+    Sample SMS Format:
+    CASHVOUCHER Name/Description/Amount
+
+    Examples:
+    cashvoucher Charlie Santos/loan/20000
+    cashvoucher Onyo Aquino/xerox/14 [in this case, Onyo is the coop officer who used his personal money to buy a xerox and then got reimbursed by the coop]
+    """
+    params = re.split(' */ *',entry['msg'])
+    #params[0] is smsCommand
+    farmerName = params[1]
+    description = params[2]
+    amount = params[3]
+    #print "CASHVOUCHER. farmerName:%s. description:%s. amount:%s." % (farmerName, description, amount)
+    try:
+        amount = float(params[3])
+    except ValueError: 
+        reply = "Could not complete cash voucher, amount entered is not numerical. "
+        reply += "Example of a valid entry would be: "+smsCommand+"/"
+        reply += farmerName+"/"+description+str(450.50)
+        return reply
+
